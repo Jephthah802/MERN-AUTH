4c6ac9d3b023e7711164f9975a41b1180ddfc17e
Jephthah802 <jephewoh@gmail.com>
2025-10-27 22:37:35 +0100

    third commit

--- api/config/mongodb.js
+++ api/config/mongodb.js
@@ -1,15 +1,26 @@
+
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log("[db.js] MongoDB already connected");
+    return;
+  }
 
-  mongoose.connection.on('connected', () => {
-    console.log('Data base coonected');
-  });
-  await mongoose.connect(`${process.env.MONGODB_URI}/sign_login_auth` )
-}
-
-
+  console.log("[db.js] Attempting initial DB connection...");
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+    console.log(`[db.js] MongoDB connected: ${conn.connection.host} | DB: ${conn.connection.name}`);
+  } catch (error) {
+    console.error("[db.js] Failed to connect to MongoDB:", error.message);
+    throw error;
+  }
+};
 
 export default connectDB;
