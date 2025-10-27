6ba6b5d904d09196df81afe1015f63a609aec1d8
Jephthah802 <jephewoh@gmail.com>
2025-10-28 00:46:17 +0100

    ten  commit

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      'https://mern-auth-53v4ild9z-jephewoh-gmailcoms-projects.vercel.app',
+      'https://mern-auth-q6tfr2sf8-jephewoh-gmailcoms-projects.vercel.app',
       'https://mern-auth-xezq.onrender.com',
       'https://mern-auth-peach-pi.vercel.app',
       'http://localhost:5173',
