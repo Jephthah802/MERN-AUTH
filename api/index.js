e53a2c36f71a15a39a397b40bb8360993e6f52ea
Jephthah802 <jephewoh@gmail.com>
2025-10-27 23:52:43 +0100

    seventh commit

--- api/index.js
+++ api/index.js
@@ -8,15 +8,25 @@ import authRouter from './routes/authrout.js';
 import userRouther from './routes/userrouts.js';
 
 const app = express();
-const port = process.env.PORT || 4000;
-connectDB();
 
-const allowedOrigins=  ['http://localhost:5173']
+app.use(
+  cors({
+    origin: [
+      'https://mern-auth-4hv0owu6a-jephewoh-gmailcoms-projects.vercel.app',
+      'https://mern-auth-xezq.onrender.com',
+      'https://mern-auth-peach-pi.vercel.app',
+      'http://localhost:5173',
+      'http://127.0.0.1:5500',
+    ],
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials:true}));
 
+connectDB();
 
 // API Endpoints
 app.get('/', (req, res) => {
@@ -24,6 +34,8 @@ app.get('/', (req, res) => {
 });
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouther);
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
